f2af8c7141d9ba724c73524341fb284fa4d0a4a6
Dat Nguyen <nthanhdat1@tma.com.vn>
2021-07-01 14:58:12 +0700

    Complete Angular Routing

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ActivatedRoute, Data, Params, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 import { ServersService } from '../servers.service';
@@ -20,14 +20,20 @@ export class ServerComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    const id = Number(this.route.snapshot.params['id']);
-    this.server = this.serversService.getServer(id);
-    this.route.params
+    this.route.data
       .subscribe(
-        (params: Params) => {
-          this.server = this.serversService.getServer(Number(params['id']));
+        (data: Data)=>{
+          this.server = data['server'];
         }
       )
+    // const id = Number(this.route.snapshot.params['id']);
+    // this.server = this.serversService.getServer(id);
+    // this.route.params
+    //   .subscribe(
+    //     (params: Params) => {
+    //       this.server = this.serversService.getServer(Number(params['id']));
+    //     }
+    //   );
   }
 
   onEdit() {
